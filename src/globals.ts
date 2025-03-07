067e4cffda877c68dbd8dc66bc2874d5749a5e5b
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-07 18:43:00 +0100

    Added available languages to globals

--- src/globals.ts
+++ src/globals.ts
@@ -2,6 +2,12 @@ export const VERSION = '0.0.1';
 export const API_BASE = 'http://localhost:4545';
 export const CDN_BASE = 'http://localhost:5555';
 
+export const LANGS = [
+    {key: 'language.english', value: 'en', disabled: false},
+    {key: 'language.german', value: 'de', disabled: false},
+    {key: 'language.french', value: 'fr', disabled: true},
+]
+
 export const DEFAULT_SETTINGS = {
     'theme-accent-color': '#71c94e',
     'prefered-content-language': 'interface',
