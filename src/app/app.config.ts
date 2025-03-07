7dda1463d0811fcb5a125ee7bdb114c1b948daf8
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-07 18:44:08 +0100

    Added Translation Services and Providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,31 @@
 import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from "@angular/common/http";
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withFetch, HttpClient } from "@angular/common/http";
 import { CookieService } from 'ngx-cookie-service';
+import { provideTranslateService, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => new TranslateHttpLoader(http, './i18n/', '.json');
+
 export const appConfig: ApplicationConfig = {
-    providers: [provideAnimations(), provideHttpClient(withFetch()), CookieService, provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), NG_EVENT_PLUGINS]
+    providers: [
+        provideAnimations(), 
+        provideHttpClient(withFetch()), 
+        CookieService, 
+        provideZoneChangeDetection({ eventCoalescing: true }), 
+        provideRouter(routes, withViewTransitions()), 
+        NG_EVENT_PLUGINS,
+        provideTranslateService({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: httpLoaderFactory,
+                deps: [HttpClient],
+            },
+        })
+    ]
 };
