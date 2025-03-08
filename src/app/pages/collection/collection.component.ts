25bc450042ad7e0cfcd7b8daaaad8e43c2745f9c
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-08 23:01:10 +0100

    Home and Collection Page added for testing

--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -3,17 +3,17 @@ import { Meta, Title } from '@angular/platform-browser';
 import { _, TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-home',
+    selector: 'app-collection',
     imports: [],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.less'
+    templateUrl: './collection.component.html',
+    styleUrl: './collection.component.less'
 })
-export class HomeComponent {
+export class CollectionComponent {
     constructor(private translate: TranslateService, private meta: Meta, private title: Title) { }
     
     ngOnInit() {
         // set title
-        this.translate.get(_('title.home')).subscribe((res: any) => {
+        this.translate.get(_('title.collection')).subscribe((res: any) => {
             this.title.setTitle(`${res} | MangaDB`);
         });
     }
