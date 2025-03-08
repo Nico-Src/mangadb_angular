27a0502674b1777c5e034e7750935736bcf00f14
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-08 23:02:30 +0100

    Added Settings to User Object + Method to update specific field

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { signal, Injectable, computed, WritableSignal } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     // null user (for when the user is not logged in)
-    nullUser: User = { id: null, slug: null, username: null, profile_image: null, role: null, age_verified: null };
+    nullUser: User = { id: null, slug: null, username: null, profile_image: null, role: null, age_verified: null, settings: null };
     // current user data
     private user:WritableSignal<User> = signal<User>(this.nullUser);
     // current user logged in status
@@ -11,6 +11,7 @@ export class AuthService {
     // getters and setters for both user data and logged in status
     public getUser = computed(() => this.user());
     public setUser = (data: User) => this.user.set(data);
+    public setUserField = (field: string, value: any) => this.user.update(user => ({ ...user, [field]: value }));
     public isLoggedIn = computed(() => this.loggedIn());
     public setLoggedIn = (data: boolean) => this.loggedIn.set(data);
 }
@@ -22,4 +23,5 @@ interface User {
     profile_image: string | null;
     role: string | null;
     age_verified: boolean | null;
+    settings: any;
 }
