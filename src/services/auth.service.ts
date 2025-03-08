e50a45c673a4e7420e53d319337159e125cf53cb
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-08 11:34:05 +0100

    Introduced Null User variable for more readable code

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,25 @@
-import { signal, Injectable, computed } from "@angular/core";
-import { Observable } from "rxjs";
+import { signal, Injectable, computed, WritableSignal } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    private user:any = signal({});
-    private loggedIn = signal(document.cookie.includes('auth_session'));
+    // null user (for when the user is not logged in)
+    nullUser: User = { id: null, slug: null, username: null, profile_image: null, role: null, age_verified: null };
+    // current user data
+    private user:WritableSignal<User> = signal<User>(this.nullUser);
+    // current user logged in status
+    private loggedIn:WritableSignal<boolean> = signal<boolean>(document.cookie.includes('auth_session'));
+    // getters and setters for both user data and logged in status
     public getUser = computed(() => this.user());
-    public setUser = (data: any) => this.user.set(data);
+    public setUser = (data: User) => this.user.set(data);
     public isLoggedIn = computed(() => this.loggedIn());
     public setLoggedIn = (data: boolean) => this.loggedIn.set(data);
+}
+
+interface User {
+    id: number | null;
+    slug: string | null;
+    username: string | null;
+    profile_image: string | null;
+    role: string | null;
+    age_verified: boolean | null;
 }
