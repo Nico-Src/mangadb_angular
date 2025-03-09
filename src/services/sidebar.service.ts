d1b0a12268e6824dfbff2acb5e02fad7e7960f52
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-09 22:08:53 +0100

    Added Bool to sidebar service to indicate if user is in admin mode

--- src/services/sidebar.service.ts
+++ src/services/sidebar.service.ts
@@ -3,6 +3,9 @@ import { signal, Injectable, computed, WritableSignal } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class SideBarService {
     private open:WritableSignal<boolean> = signal<boolean>(true);
+    private inAdminArea:WritableSignal<boolean> = signal<boolean>(false);
     public isOpen = computed(() => this.open())
     public setOpen = (data: boolean) => this.open.set(data)
+    public isInAdminArea = computed(() => this.inAdminArea())
+    public setInAdminArea = (data: boolean) => this.inAdminArea.set(data)
 }
