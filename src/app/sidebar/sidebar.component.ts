1f6540db4919d6c441d043acb22610b5d183f14f
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-09 22:08:22 +0100

    Introduced event listener to listen to changes to the route

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
-import { Component, computed } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, computed, effect, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { TuiIcon, TuiHint } from '@taiga-ui/core';
 import { TuiFade } from '@taiga-ui/kit';
 import { SideBarService } from '../../services/sidebar.service';
 import { TranslatePipe } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'side-bar',
@@ -15,7 +16,14 @@ import { RouterLink } from '@angular/router';
 })
 export class SideBar {
     inAdminArea = false;
-    constructor(public router: Router, public sidebar: SideBarService) { }
+    constructor(public router: Router, public sidebar: SideBarService) {
+        // listen to navigationEnd events to update the inAdminArea variable
+        this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
+            // check if the current route starts with '/admin'
+            this.inAdminArea = this.router.url.startsWith('/admin');
+            sidebar.setInAdminArea(this.inAdminArea);
+        });
+    }
 
     ngOnInit() {
         
