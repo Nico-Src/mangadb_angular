6e4ad2cb37a45bd636054060617e64607e1f4a38
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-11 09:26:06 +0100

    Added Global Methos for Formatting Dates and other date utils

--- src/globals.ts
+++ src/globals.ts
@@ -1,11 +1,21 @@
+import { ANIMATION_MODULE_TYPE } from "@angular/core";
 import { TuiAlertService } from "@taiga-ui/core";
+import moment from 'moment';
+import 'moment/locale/de';
+import 'moment/locale/ja';
 
 export const API_BASE = 'http://localhost:4545';
 export const CDN_BASE = 'http://localhost:5555';
 
 export const CONFIG = {
     version: '0.0.1',
-    default_locale: 'en'
+    default_locale: 'en',
+    auth: {
+        min_username_length: 3,
+        max_username_length: 31,
+        min_password_length: 6,
+        max_password_length: 255
+    }
 };
 
 export const LANGS = [
@@ -22,10 +32,84 @@ export const DEFAULT_SETTINGS = {
     'view-mode': 'column',
 };
 
+export const ANNOUNCED_DATE = "date.announced";
+export const UNKNOWN_DATE = "date.unknown";
+
+export const dateFormats = [
+    {locale: 'de', format: 'DD.MM.YYYY'},
+    {locale: 'en', format: 'DD-MM-YYYY'},
+    {locale: 'de_write_out', format: 'Do MMMM YYYY'},
+    {locale: 'en_write_out', format: 'Do MMMM YYYY'},
+    {locale: 'ja', format: 'DD日MM月YYYY年'},
+    {locale: 'ja_write_out', format: 'DoMM月YYYY年'},
+];
+
+export const dateTimeFormats = [
+    {locale: 'de', format: 'LLL'},
+    {locale: 'en', format: 'LLL'},
+    {locale: 'ja', format: 'LLL'},
+];
+
 export const errorAlert = (alerts: TuiAlertService, message: string, label: string = 'Error'): void => {
     alerts.open(message, {label: label, appearance: 'negative'}).subscribe();
 }
 
 export const successAlert = (alerts: TuiAlertService, message: string, label: string = 'Success'): void => {
     alerts.open(message, {label: label, appearance: 'positive'}).subscribe();
+}
+
+// Convert Hex Color to RGB
+export const hexToRGB = (hex: string) => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
+
+// Convert date to a human-readable format based on current locale
+export const readableDate = (str: string,lang: string,writeOut: boolean) => {
+    const locale = lang;
+    if(!str) return null;
+    const parsed = moment.utc(str);
+    // year before 1100 is considered unknown
+    // year after 2100 is considered announced (not yet known)
+    if(parsed.year() < 1100) return UNKNOWN_DATE;
+    if(parsed.year() >= 2100) return ANNOUNCED_DATE;
+
+    // check if the date should be written out (31st december instead of 31-12)
+    if(!writeOut) return parsed.locale(locale).format(dateFormats.find(f => f.locale === locale)?.format || 'YYYY-MM-DD');
+    else return parsed.locale(locale).format(dateFormats.find(f => f.locale === `${locale}_write_out`)?.format || 'Do MMMM YYYY');
+};
+
+export const readableDatetime = (str: string,loc: string) => {
+    const locale = loc;
+    if(!str) return null;
+    const parsed = moment.utc(str);
+    // year before 1100 is considered unknown
+    // year after 2100 is considered announced (not yet known)
+    if(parsed.year() < 1100) return UNKNOWN_DATE;
+    if(parsed.year() >= 2100) return ANIMATION_MODULE_TYPE;
+
+    // check if the date should be written out (31st december instead of 31-12)
+    return parsed.locale(locale).format(dateTimeFormats.find(f => f.locale === locale)?.format || 'en');
+}
+
+export const langToLocale = (lang:string) => {
+    switch(lang){
+        case "German": return "de";
+        case "English": return "en";
+        case "French": return "fr";
+        case "Spanish": return "es";
+        case "Italian": return "it";
+        case "Korean": return "kor";
+        case "Chinese": return "cn";
+        case "Japanese": return "jpn";
+        default: return "en";
+    }
+}
+
+export const isDateInFuture = (date:string) => {
+    return moment(date).isAfter(moment(new Date));
 }
