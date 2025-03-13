ecbdff55431fc0c303eeb8810cda40e7b3134c48
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-13 17:15:24 +0100

    Icons in Sidebar replaced

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,18 +1,25 @@
 import { Component, computed, effect, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { TuiIcon, TuiHint } from '@taiga-ui/core';
+import { TuiHint } from '@taiga-ui/core';
 import { TuiFade } from '@taiga-ui/kit';
 import { SideBarService } from '../../services/sidebar.service';
 import { TranslatePipe } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { tablerHome, tablerListSearch, tablerQuote } from '@ng-icons/tabler-icons';
+import { solarLibrary, solarHistory, solarBillList, solarSettings, solarUsersGroupRounded, solarCart4, solarFileText } from '@ng-icons/solar-icons/outline';
+import { solarBuildingsLinear } from '@ng-icons/solar-icons/linear';
+import { lucideDices } from '@ng-icons/lucide';
+import { matPrivacyTipOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
     selector: 'side-bar',
-    imports: [NgIf, TuiIcon, TuiFade, TuiHint, TranslatePipe, RouterLink],
+    imports: [NgIf, TuiFade, TuiHint, TranslatePipe, RouterLink, NgIcon],
     templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.less'
+    styleUrl: './sidebar.component.less',
+    viewProviders: [provideIcons({ tablerHome, solarLibrary, solarHistory, solarBillList, solarSettings, tablerListSearch, solarBuildingsLinear, lucideDices, solarUsersGroupRounded, solarCart4, tablerQuote, matPrivacyTipOutline, solarFileText })]
 })
 export class SideBar {
     inAdminArea = false;
