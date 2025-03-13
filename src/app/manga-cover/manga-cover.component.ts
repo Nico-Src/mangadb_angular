f967d150d8ee61cf30f939a537d047c4d5d085e8
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-13 15:21:35 +0100

    Manga Cover Component added

--- src/app/manga-cover/manga-cover.component.ts
+++ src/app/manga-cover/manga-cover.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input } from '@angular/core';
-import { CDN_BASE } from '../../globals';
+import { Component, Input, signal } from '@angular/core';
+import { CDN_BASE, DEFAULT_SETTINGS } from '../../globals';
 import { NgIf } from '@angular/common';
 import { TuiImgLazyLoading } from '@taiga-ui/kit';
 import { TuiSkeleton } from '@taiga-ui/kit';
+import { AuthService } from '../../services/auth.service';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { tablerRating16Plus, tablerRating18Plus } from '@ng-icons/tabler-icons';
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'manga-cover',
-    imports: [NgIf, TuiImgLazyLoading, TuiSkeleton],
+    imports: [NgIf, TuiImgLazyLoading, TuiSkeleton, NgIcon, TranslatePipe],
     templateUrl: './manga-cover.component.html',
-    styleUrl: './manga-cover.component.less'
+    styleUrl: './manga-cover.component.less',
+    viewProviders: [provideIcons({ tablerRating16Plus, tablerRating18Plus })]
 })
 export class MangaCover {
     readonly cdn_base = CDN_BASE;
+    constructor(private auth: AuthService, private translate: TranslateService){}
     loading = true;
     @Input() id: number = -1;
     @Input() fit: string = 'default';
     @Input() blur: boolean = false;
     @Input() aspectRatio: number = 1;
+    @Input() nsfw: boolean = false;
+    @Input() nsfw18: boolean = false;
+    @Input() forceSkeleton: boolean = false;
+    needsNSFWPlaceholder = signal(false);
+    needsNSFW18Placeholder = signal(false);
+    needsSkeleton = signal(false);
+
+    ngOnInit(){
+        let nsfwMode = this.auth.getUserSetting('nsfw-mode');
+        let ageVerified = false;
+        if(this.auth.isLoggedIn()){
+            ageVerified = this.auth.getUser().age_verified || false;
+        }
+        this.needsNSFWPlaceholder.set(this.nsfw && nsfwMode === 'settings.nsfw.hide-nsfw');
+        this.needsNSFW18Placeholder.set((this.nsfw18 && (nsfwMode === 'settings.nsfw.hide-nsfw' || nsfwMode === 'settings.nsfw.show-nsfw')) || (this.nsfw18 && ageVerified === false));
+        this.needsSkeleton.set((this.needsNSFWPlaceholder() === false && this.needsNSFW18Placeholder() === false) || this.forceSkeleton === true);
+    }
 }
