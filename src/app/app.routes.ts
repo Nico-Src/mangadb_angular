d27dfb882efac6710fced41bd8b37d0a5e0fadfb
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-18 16:27:31 +0100

    More Routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,54 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AdminGuard } from '../guards/adminGuard';
+import { AuthGuard } from '../guards/authGuard';
+import { BrowseSeriesComponent } from './pages/browse-series/browse-series.component';
+import { SeriesDetailComponent } from './pages/series-detail/series-detail.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+        canActivate: [AuthGuard],
     },
     {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [AuthGuard],
     },
     {
         path: 'register',
-        component: RegisterComponent
+        component: RegisterComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'collection',
+        component: CollectionComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'browse-series',
+        component: BrowseSeriesComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'series/:slug',
+        component: SeriesDetailComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'admin/dashboard',
+        component: DashboardComponent,
+        canActivate: [AdminGuard],
     },
     // auto redirect
     { 
