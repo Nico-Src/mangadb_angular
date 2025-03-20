1f2cb861fd95bb476b420b0c5f8223599d138257
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-20 22:30:51 +0100

    AuthGuard documented

--- src/guards/authGuard.ts
+++ src/guards/authGuard.ts
@@ -1,11 +1,12 @@
-import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterState, RouterStateSnapshot } from "@angular/router";
+import { CanActivate } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 import { inject, Injectable } from "@angular/core";
 
+// Guard to authenticate user before navigating
 @Injectable()
 export class AuthGuard implements CanActivate{
     private auth = inject(AuthService);
-    constructor(private router: Router){}
+
     async canActivate(){
         await this.auth.getSessionAsync();
         return true;
