4acaccdfa044ceecf78c8174e7ad9a3dab3a7eee
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-20 22:29:13 +0100

    Service added for API Access

---
+++ src/services/api.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from "@angular/common/http";
+import { API_BASE, DEFAULT_SETTINGS } from "../globals";
+import { Injectable, inject } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { AuthService } from "./auth.service";
+
+@Injectable({ providedIn: 'root' })
+export class APIService {
+    private http:HttpClient = inject(HttpClient);
+    private cookie:CookieService = inject(CookieService);
+    
+    // request method
+    public request<T>(method: HttpMethod, url: string, params: any, responseType: string = 'json'){
+        const headers:any = {};
+        const session = this.cookie.get('auth_session');
+        const loggedIn:boolean = session != undefined && session != null;
+        // if user is logged in then add authorization header
+        if(loggedIn) headers['Authorization'] = "Bearer " + this.cookie.get('auth_session');
+        // based on given http method execute
+        switch(method){
+            case HttpMethod.GET:
+                return this.http.get<T>(`${API_BASE}/${url}`, {responseType: responseType as "json", headers: headers});
+            case HttpMethod.POST:
+                return this.http.post<T>(`${API_BASE}/${url}`, params, {responseType: responseType as "json", headers: headers});
+            case HttpMethod.DELETE:
+                return this.http.delete<T>(`${API_BASE}/${url}`, {responseType: responseType as "json", headers: headers});
+            case HttpMethod.PUT:
+                return this.http.put<T>(`${API_BASE}/${url}`, params, {responseType: responseType as "json", headers: headers});
+            case HttpMethod.PATCH:
+                return this.http.patch<T>(`${API_BASE}/${url}`, params, {responseType: responseType as "json", headers: headers});
+            default:
+                return this.http.get<T>(``,{responseType: responseType as "json", headers: headers});
+        }
+    }
+}
+
+export enum HttpMethod{
+    GET,
+    POST,
+    DELETE,
+    PUT,
+    PATCH
+}
