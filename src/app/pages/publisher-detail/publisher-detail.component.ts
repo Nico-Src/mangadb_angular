18faf8cb1799771de6555564111345fd90cfe444
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-28 10:47:17 +0100

    Publisher Detail Page finished (Published Works & Related Publishers)

--- src/app/pages/publisher-detail/publisher-detail.component.ts
+++ src/app/pages/publisher-detail/publisher-detail.component.ts
@@ -1,38 +1,43 @@
 import { Component, computed, ElementRef, HostListener, inject, Input, ViewChild } from '@angular/core';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
-import { TuiButton, TuiAppearance, TuiLoader, TuiAlertService, TuiTextfield, tuiDateFormatProvider } from '@taiga-ui/core';
-import { TuiElasticContainer, TuiFade, TuiSkeleton } from '@taiga-ui/kit';
+import { TuiAppearance, TuiLoader, TuiAlertService, TuiTextfield, tuiDateFormatProvider } from '@taiga-ui/core';
+import { TuiElasticContainer, TuiFade, TuiPagination, TuiSegmented, TuiSkeleton } from '@taiga-ui/kit';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { APIService, HttpMethod } from '../../../services/api.service';
 import { AuthService } from '../../../services/auth.service';
-import { errorAlert, successAlert, getTranslation, CDN_BASE, readableDate, UNKNOWN_DATE, ANNOUNCED_DATE, localeToLang, LANGS, langToLocale } from '../../../globals';
+import { CDN_BASE, readableDate, UNKNOWN_DATE, ANNOUNCED_DATE, localeToLang, LANGS, langToLocale } from '../../../globals';
 import { TuiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { matHistoryOutline } from '@ng-icons/material-icons/outline';
 import { solarBuildings2Linear } from '@ng-icons/solar-icons/linear';
 import { solarDoubleAltArrowDown, solarDoubleAltArrowUp, solarLink } from '@ng-icons/solar-icons/outline';
 import { LinkWarnDialog } from '../../link-warn-dialog/link-warn-dialog.component';
 import { heroDocumentMagnifyingGlass } from '@ng-icons/heroicons/outline';
+import { tablerSortAscendingLetters, tablerSortDescendingLetters, tablerSortAscending, tablerSortDescending, tablerList, tablerLayoutColumns, tablerLayoutGrid } from '@ng-icons/tabler-icons';
+import { MangaSeriesColumnComponent } from '../../manga-series-column/manga-series-column.component';
+import { MangaSeriesGridComponent } from '../../manga-series-grid/manga-series-grid.component';
+import { MangaSeriesListComponent } from '../../manga-series-list/manga-series-list.component';
 
 @Component({
     selector: 'publisher-detail',
-    imports: [ TranslatePipe, TuiButton, LinkWarnDialog, TuiAppearance, TuiElasticContainer, NgIcon, NgIf, NgFor, TuiFade, TuiLoader, ReactiveFormsModule, TuiSkeleton, FormsModule, TuiTextfield, TuiSelectModule, TuiTextfieldControllerModule],
+    imports: [ TranslatePipe, MangaSeriesColumnComponent, MangaSeriesGridComponent, MangaSeriesListComponent, LinkWarnDialog, TuiSegmented, TuiPagination, TuiAppearance, TuiElasticContainer, NgIcon, NgIf, NgFor, TuiFade, TuiLoader, ReactiveFormsModule, TuiSkeleton, FormsModule, TuiTextfield, TuiSelectModule, TuiTextfieldControllerModule],
     templateUrl: './publisher-detail.component.html',
     styleUrl: './publisher-detail.component.less',
     providers: [tuiDateFormatProvider({mode: 'YMD', separator: '/'})],
-    viewProviders: [provideIcons({ solarBuildings2Linear, matHistoryOutline, solarLink, heroDocumentMagnifyingGlass, solarDoubleAltArrowDown, solarDoubleAltArrowUp })]
+    viewProviders: [provideIcons({ solarBuildings2Linear, matHistoryOutline, solarLink, heroDocumentMagnifyingGlass, solarDoubleAltArrowDown, solarDoubleAltArrowUp, tablerSortAscendingLetters, tablerSortDescendingLetters, tablerSortAscending, tablerSortDescending, tablerList, tablerLayoutColumns, tablerLayoutGrid })]
 })
 export class PublisherDetailComponent {
     private readonly api = inject(APIService);
     private readonly auth = inject(AuthService);
     private readonly alerts = inject(TuiAlertService);
     readonly theme = computed(() => this.auth.theme());
-    constructor(private translate: TranslateService, private el: ElementRef, private title: Title, private route: ActivatedRoute){}
+    constructor(private translate: TranslateService, private el: ElementRef, private title: Title, private route: ActivatedRoute, private router: Router){}
     readonly cdn_base = CDN_BASE;
     publisher: any = {};
+    works: any = [];
     imgLoading: boolean = true;
     show404: boolean = false;
     linkDialogURL: string = "";
@@ -43,6 +48,21 @@ export class PublisherDetailComponent {
     expandDesc: boolean = false;
     checkingDescOverflow: boolean = true;
 
+    search:string = "";
+    currentSearch:string = "";
+    prevSearch:string = "";
+    currentPage:number = 0;
+    maxPages:number= 10;
+    loading:boolean = true;
+    viewIndex:number = 0;
+    orders:any = [
+        {key: 'name', value: 'name-asc', icon: 'tablerSortAscendingLetters'},
+        {key: 'name', value: 'name-desc', icon: 'tablerSortDescendingLetters'},
+        {key: 'added', value: 'added-asc', icon: 'tablerSortAscending'},
+        {key: 'added', value: 'added-desc', icon: 'tablerSortDescending'},
+    ]
+    selectedOrder: any = this.orders[0];
+
     @ViewChild('content') content: any;
     @ViewChild('publisherTitle') publisherTitle: any;
     @ViewChild('desc') desc: any;
@@ -50,9 +70,51 @@ export class PublisherDetailComponent {
 
     async ngOnInit(){
         this.title.setTitle(`Publisher | MangaDB`);
+
+        // get query params (also on change)
+        this.route.queryParams.subscribe(params => {
+            if(params['order']) this.selectedOrder = this.orders.find((o: { value: any; }) => o.value === params['order']);
+            if(params['page']) this.currentPage = parseInt(params['page']);
+            if(params['search']) this.search = this.prevSearch = this.currentSearch = params['search'];
+        });
+
+        // get users view mode (list, column or grid)
+        this.viewIndex = parseInt(localStorage.getItem('viewMode') || '0');
+
+        this.route.paramMap.subscribe(()=>{
+            if(!this.publisher?.id) return;
+            this.currentPage = 0;
+            this.search = this.currentSearch = this.prevSearch = "";
+            this.selectedOrder = this.orders[0];
+            this.imgLoading = true;
+            this.loading = true;
+            this.show404 = false;
+
+            // reset description state (not expanded, max height and remove clamp)
+            this.expandDesc = false;
+            this.desc.nativeElement.style.maxHeight = '120px';
+            this.desc.nativeElement.classList.remove('clamp');
+            // check overflow for new description
+            this.checkingDescOverflow = true;
+
+            this.content.nativeElement.classList.remove('fits');
+
+            const slug = this.route.snapshot.paramMap.get('slug');
+            this.loadPublisher(slug);
+        });
+
         // get route param (slug)
+        this.updateQueryParams(true);
+    }
+
+    // update query params
+    updateQueryParams(first: boolean = false){
+        // navigate router without reloading and without pushing to history
+        this.router.navigate([], { queryParams: {order: this.selectedOrder.value, page: this.currentPage, search: this.search.trim() !== '' ? this.search : null}, queryParamsHandling: 'merge', replaceUrl: true });
+        // reload series after
         const slug = this.route.snapshot.paramMap.get('slug');
-        this.loadPublisher(slug);
+        if(first) this.loadPublisher(slug);
+        else this.loadWorks();
     }
 
     // load publisher data from api
@@ -62,7 +124,6 @@ export class PublisherDetailComponent {
         const lang = contentLang === 'interface' ? this.translate.currentLang || 'en' : contentLang;
 
         this.api.request<any>(HttpMethod.GET, `publisher/slug/${slug}?user_lang=${lang}`, {}).subscribe((res:any)=>{
-            console.log(res)
             this.publisher = res;
             this.title.setTitle(`${(this.publisher.name || 'No Name')} | MangaDB`);
             // convert founding date to readable format
@@ -82,6 +143,8 @@ export class PublisherDetailComponent {
             this.availableDescLangs = LANGS.filter(l => this.publisher?.descriptions.find((d: { language: string; }) => langToLocale(d.language) === l.value));
             this.selectedDescLang = this.availableDescLangs.find((l: { value: string; }) => l.value === langToLocale(description?.language || 'English'));
 
+            this.loadWorks();
+
             setTimeout(()=>{
                 // fit title and alias to container and check overflow for current description
                 this.fitToParent(this.publisherTitle?.nativeElement,{max: 50, height: 120});
@@ -93,6 +156,40 @@ export class PublisherDetailComponent {
             },500);
         }, (err:any)=>{
             this.show404 = true;
+            this.loading = false;
+        });
+    }
+
+    // load published works
+    loadWorks(){
+        const LIMIT = 12;
+
+        // get users prefered content language
+        const contentLang = this.auth.getUserSetting('prefered-content-language');
+        const lang = contentLang === 'interface' ? this.translate.currentLang || 'en' : contentLang;
+
+        const offset = this.currentPage * LIMIT;
+
+        this.api.request<any>(HttpMethod.POST, `publisher/publications/${this.publisher.id}?user_lang=${lang}`, {order: this.selectedOrder.value, limit: 12, offset, search: this.search, user_lang: lang}).subscribe((res:any)=>{
+            this.maxPages = res.max;
+
+            // check if aliases were matched in backend
+            for(const item of res.publications){
+                console.log(item)
+                // if there are no aliases or name matches search keep it as it is
+                if(!item.aliases || item.name?.toLowerCase()?.includes(this.search.toLocaleLowerCase())) continue;
+                // else check aliases and add with found alias as name
+                const aliases = (item.aliases as string).split(',');
+                for(const alias of aliases){
+                    if(alias.toLowerCase().includes(this.search.toLowerCase())){
+                        item.alias = alias;
+                        break;
+                    }
+                }
+            }
+
+            this.works = res.publications;
+            this.loading = false;
         });
     }
 
@@ -197,5 +294,35 @@ export class PublisherDetailComponent {
             case "Canada": return "ca";
             default: return 'en';
         }
+    }
+
+    // view mode changed
+    viewChanged(index: number) {
+        // save to local storage
+        localStorage.setItem('viewMode', index.toString());
+    }
+
+    // keydown handler for search input
+    searchKeyDown(event: KeyboardEvent) {
+        if(event.key === 'Enter') {
+            // check if search is different from previous search
+            if(this.search !== this.prevSearch) {
+                this.currentPage = 0;
+                this.prevSearch = this.search;
+                this.currentSearch = this.search;
+                // update query params
+                this.updateQueryParams();
+            }
+        }
+    }
+
+    // redirect to other publisher
+    relationClick(slug:string){
+        this.router.navigate(['publisher',slug]);
+    }
+
+    // redirect to series
+    seriesClick(slug:string){
+        this.router.navigate(['series',slug]);
     }
 }
