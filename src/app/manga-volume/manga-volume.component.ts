3ca5bd7e22784eae5ee6fa4aeca29b3b4ffec598
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-03-30 15:58:29 +0200

    Added Context Menu to Manga Volumes

--- src/app/manga-volume/manga-volume.component.ts
+++ src/app/manga-volume/manga-volume.component.ts
@@ -1,25 +1,40 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { CDN_BASE, readableDate, langToLocale, ANNOUNCED_DATE, UNKNOWN_DATE, isDateInFuture } from '../../globals';
-import { NgIf } from '@angular/common';
+import { Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
+import { CDN_BASE, readableDate, langToLocale, ANNOUNCED_DATE, UNKNOWN_DATE, isDateInFuture, errorAlert, successAlert } from '../../globals';
+import { NgFor, NgIf } from '@angular/common';
 import { MangaCover } from '../manga-cover/manga-cover.component';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { solarNotebook, solarCalendar } from '@ng-icons/solar-icons/outline';
 import { lucideLibrary } from '@ng-icons/lucide';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
+import { TuiAlertService, TuiDataList, TuiDropdown } from '@taiga-ui/core';
+import { TuiDataListDropdownManager } from '@taiga-ui/kit';
+import { AuthService } from '../../services/auth.service';
+import { APIService, HttpMethod } from '../../services/api.service';
+import { heroMinus, heroPlus, heroViewfinderCircle } from '@ng-icons/heroicons/outline';
 
 @Component({
     selector: 'manga-volume',
-    imports: [MangaCover, NgIcon, TranslatePipe, NgIf],
+    imports: [MangaCover, NgIcon, NgFor, TuiDataList, TuiDataListDropdownManager, TuiDropdown, TranslatePipe, NgIf],
     templateUrl: './manga-volume.component.html',
     styleUrl: './manga-volume.component.less',
-    viewProviders: [provideIcons({ solarNotebook, solarCalendar, lucideLibrary })]
+    viewProviders: [provideIcons({ solarNotebook, solarCalendar, lucideLibrary, heroViewfinderCircle, heroPlus, heroMinus })]
 })
 export class MangaVolume {
+    private readonly api = inject(APIService);
+    private readonly auth = inject(AuthService);
+    private readonly alerts = inject(TuiAlertService);
     readonly cdn_base = CDN_BASE;
     loading = true;
     @Input() volume:any = {};
     @Input() showRelease = true;
     @ViewChild('release') release:any;
+    @ViewChild('dropdown') dropdown:any;
+
+    protected readonly menuItems = [
+        {title: 'quick-view', icon: 'heroViewfinderCircle', action: this.quickView.bind(this)},
+        {title: 'toggle-collection', icon: '', action: this.toggleCollection.bind(this)},
+    ] as const;
+    
 
     constructor(private translate: TranslateService){}
 
@@ -44,5 +59,26 @@ export class MangaVolume {
     // check if date is in future
     preOrder(date:string){
         return isDateInFuture(date);
+    }
+
+    quickView(){
+        
+    }
+
+    // toggle collection state
+    toggleCollection(){
+        // if not logged in show error
+        if(!this.auth.isLoggedIn()){
+            // TODO translate
+            errorAlert(this.alerts, 'You need to be logged in to do this.', undefined, this.translate);
+            return;
+        }
+
+        this.api.request<any>(HttpMethod.GET, `volumes/toggle-collection/id/${this.volume.id}`, {}).subscribe((res)=>{
+            this.volume.collected = res;
+            // TODO translate
+            if(res) successAlert(this.alerts, 'Successfully added to collection.', undefined, this.translate);
+            else successAlert(this.alerts, 'Successfully removed from collection.', undefined, this.translate);
+        });
     }
 }
