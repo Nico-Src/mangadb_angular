d5dec3a4e1ab8bd92ccd41c4ebc2bdad36d7efb0
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-04-12 14:27:26 +0200

    Added function to set scroll value globally

--- src/services/sidebar.service.ts
+++ src/services/sidebar.service.ts
@@ -5,8 +5,10 @@ export class SideBarService {
     private open:WritableSignal<boolean> = signal<boolean>(true);
     private inAdminArea:WritableSignal<boolean> = signal<boolean>(false);
     scrollTop:WritableSignal<number> = signal<number>(0);
+    public onSetScroll:any;
     public isOpen = computed(() => this.open())
     public setOpen = (data: boolean) => this.open.set(data)
     public isInAdminArea = computed(() => this.inAdminArea())
-    public setInAdminArea = (data: boolean) => this.inAdminArea.set(data)
+    public setInAdminArea = (data: boolean) => this.inAdminArea.set(data);
+    public setScrollTop = (scroll:number) => this.onSetScroll(scroll);
 }
